863613352c718123a52aada202c34849305f8659
n3kit4 <62533156+n3kit4@users.noreply.github.com>
2023-10-28 02:21:46 +0500

    Create task2.cpp

---
+++ 28_10_23/task2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Numbers (from 35 to 86) that are divisible by 7 with a remainder of 1, 2 or 5" << endl;
+    for (a = 35; a < 87; a++)
+    {
+        int remainder = a % 7;
+        if (remainder == 1 or remainder == 2 or remainder == 5)
+            cout << a << endl;
+    }
+    return 0;
+}

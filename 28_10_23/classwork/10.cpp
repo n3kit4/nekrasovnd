9bf8490adaf9d6265129533ae3617004e9cc1cf9
n3kit4 <62533156+n3kit4@users.noreply.github.com>
2023-10-29 23:19:42 +0500

    Create 10.cpp

---
+++ 28_10_23/classwork/10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int l = 5;
+    int i;
+    double a[l];
+    cout << "Введите 5 элементов массива через Enter: " << endl;
+    for (i = 0; i < l; i++)
+        cin >> a[i];
+    cout << "Элементы массива, которые больше предыдущего: " << endl;
+    for (i = 1; i < (l - 1); i++)
+        if (a[i] > a[i - 1])
+            cout << a[i] << " (> " << a[i - 1] << ")\n";
+}

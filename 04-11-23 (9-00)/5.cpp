6f9377698a73efd881ca4ff9876bed2769415e63
n3kit4 <62533156+n3kit4@users.noreply.github.com>
2023-11-06 23:49:51 +0500

    Create 5.cpp

---
+++ 04-11-23 (9-00)/5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	double x, y, y1;
+	cout << "Введите x: ";
+	cin >> x;
+	y = x;
+	y1 = x;
+	do
+	{
+		y = y1;
+		y1 = 0.5 * (y + 3 * x / (2 * y * y + x / y));
+	}
+	while (abs(y1 - y) > 1/100000);
+	cout << "Корень кубический из x = " << y;
+}

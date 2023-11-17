f0925c6485c78aad64f6911338dd1ea773f6dfd8
n3kit4 <62533156+n3kit4@users.noreply.github.com>
2023-11-18 03:09:44 +0500

    Create seminar.cpp

---
+++ 18-11-23___09-00/seminar.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int length;
+	cin >> length;
+	int* a = new int[length];
+	for (int i = 0; i < length; i++)
+	{
+		cin >> a[i];
+	}
+	int i, j, q;
+	cin >> q;
+	int* g = new int[q];
+	for (int f = 0; f < q; f++)
+	{
+		cin >> i >> j;
+		i--;
+		j--;
+		int sum = 0;
+		for (i; i <= j; i++)
+			sum += a[i];
+		g[f] = sum;
+	}
+	for (int i = 0; i < q; i++)
+		cout << g[i] << endl;
+}

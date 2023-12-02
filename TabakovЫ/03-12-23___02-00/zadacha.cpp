85e8c71b97e9fe41a708caacc6a60bf3c5bb0b98
n3kit4 <62533156+n3kit4@users.noreply.github.com>
2023-12-03 02:00:20 +0500

    Update zadacha.cpp

--- TabakovЫ/03-12-23___02-00/zadacha.cpp
+++ TabakovЫ/03-12-23___02-00/zadacha.cpp
@@ -1,1 +1,45 @@
-123
+#include <iostream> 
+ 
+using namespace std; 
+void Memory(int n) 
+{ 
+ double* array = new double[n]; 
+} 
+void CinArray(int n, double *array) 
+{ 
+ for (int i = 0; i < n; ++i) 
+  cin >> *(array + i); 
+} 
+ 
+void CoutArray(int n, double *array) 
+{ 
+ for (int i = 0; i < n; ++i) 
+  cout << *(array + i) << " "; 
+} 
+ 
+void DeleteMemory(double *array) 
+{ 
+ delete[] array; 
+} 
+int main() 
+{ 
+ while (true) 
+ { 
+  cout << "Do you want to continue?" << endl; 
+  cout << "Enter '+' or '-'" << endl; 
+  char answer; 
+  cin >> answer; 
+  if (answer == '+') 
+  { 
+   int n;; 
+   double array[1]; 
+   cin >> n; 
+   Memory(n); 
+   CinArray(n, array); 
+   CoutArray(n, array); 
+   //DeleteMemory(array); 
+  } 
+  else 
+   break; 
+ } 
+}

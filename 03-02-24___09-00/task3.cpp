22a6ac3413d9cc864263a1eeb1674553690157e4
n3kit4 <62533156+n3kit4@users.noreply.github.com>
2024-03-02 03:03:07 +0500

    Update task3.cpp

--- 03-02-24___09-00/task3.cpp
+++ 03-02-24___09-00/task3.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
-    long long int num = 7435421243;
+    float num = 7435421243;
     for (int i = 0; i < 3; i++) {
-        num /= 1024;
-        cout << num << endl;
+        num /= 1024; 
+        cout << num << "\t";
+        printf("%.5f\n", num);
     }
 }
